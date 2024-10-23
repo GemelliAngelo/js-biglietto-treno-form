ea1858279ae3b6349f74fe95793f8ca0590cfeb7
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-23 15:58:28 +0200

    add structure

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-const fullnameinput = document.getElementById("name");
+const nameinput = document.getElementById("name");
+const surnameinput = document.getElementById("surname");
 const distanceinput = document.getElementById("distance");
+const ageinput = document.getElementById("age");
+const generateButton = document.getElementById("generate-button");
+const pricePerKm = 0.21;
+
+generateButton.addEventListener("click", () => {
+  const ticketPrice = parseInt(distanceinput.value) * pricePerKm;
+});
