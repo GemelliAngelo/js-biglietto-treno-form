8244ae9a11a0f64fedb3b2f61dd59d409236df22
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-23 16:41:24 +0200

    add card

--- js/main.js
+++ js/main.js
@@ -3,8 +3,17 @@ const surnameinput = document.getElementById("surname");
 const distanceinput = document.getElementById("distance");
 const ageinput = document.getElementById("age");
 const generateButton = document.getElementById("generate-button");
+const undoButton = document.getElementById("undo-button");
+const fullName = document.getElementById("fullname");
 const pricePerKm = 0.21;
 
 generateButton.addEventListener("click", () => {
-  const ticketPrice = parseInt(distanceinput.value) * pricePerKm;
+  let ticketPrice = parseInt(distanceinput.value) * pricePerKm;
+  if (parseInt(ageinput.value) < 18) {
+    ticketPrice = ticketPrice - ticketPrice * 0.2;
+  } else if (parseInt(ageinput.value) >= 65) {
+    ticketPrice = ticketPrice - ticketPrice * 0.4;
+  }
+  //   document.getElementById("price").innerHTML = `€ ${ticketPrice.toFixed(2)}`;
+  fullName.innerHTML = nameinput.value + " " + surnameinput.value;
 });
